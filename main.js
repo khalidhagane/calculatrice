ce9afaab4b6decf7caf58816b98803312de73c98
khalid hagane <khalidhagane62@gmail.com>
2022-03-10 11:08:33 +0100

    ajoute les fiches html , css , js et design

---
+++ main.js
@@ -0,0 +1,18 @@
+// let ecran = document.getElementById('ecran');
+// let buttons = Array.from( document.getElementsByClassName('button'));
+// console.log(buttons);
+// function calculer() 
+// { let a = document.getElementById("output").value 
+//     // let b = eval(a) 
+//     document.getElementById("output").value = b
+//     } 
+
+
+
+
+// parseFloat(ecran.value)
+
+    // function effacer() 
+    // { 
+    //     document.getElementById("output").value = "" 
+    // } 
