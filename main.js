e2bdb631b8bfb60ae441711042553f7ff3893c07
khalid hagane <khalidhagane62@gmail.com>
2022-03-15 09:24:21 +0100

    calculatrice

--- main.js
+++ main.js
@@ -1,30 +1,66 @@
-// let ecran = document.getElementById('ecran');
-// let buttons = Array.from( document.getElementsByClassName('button'));
-// console.log(buttons);
-// function calculer() 
-// { let a = document.getElementById("output").value 
-//     // let b = eval(a) 
-//     document.getElementById("output").value = b
-//     } 
-let ecran = document.getElementById("output") 
-ecran.value = 0;
-function afficher(val) 
-{ 
-    ecran.value += val
-    // ecran.value=parseFloat(ecran.value)
-    // document.getElementById("output").value+=val
+
+let ecran = document.getElementById("output");
+let arr = [];
+let input = 0;
+
+function affichage(number){
+    ecran.value = ecran.value + number
+    input = input + number
+    
+    // (input + number) => fkola mara hat liya number kitzad 3la number li 9ablo okithat liya f case wahda ma array 
+    // console.log(input)
 }
 
-let luka = document.getElementById('efacer')
-luka.addEventListener('click', function(){
-ecran.value=0;
+let efacer = document.getElementById('suprimer')
+efacer.addEventListener('click',function()
+// function suprimer()
+{
+    ecran.value = "";
+    input= 0;
+    arr = [];
+    // console.log(input)
 })
 
+let operateurs = document.querySelectorAll('.operateur')
+operateurs.forEach(traite => {
+    traite.addEventListener('click',function(){
+        let content = traite.getAttribute('content')
+        // function traiter(operateur)
+        if(input !== 0 ){
+        //ecran.value = ecran.value + operateur; 
+        ecran.value+=content;
+        input= parseFloat(input);
+        arr.push(input);
+        arr.push(content);
+        input = 0;
+        // console.log(arr);
+        // console.log(input);
+    }
+    });
+});
 
+function egale(arr){
+    if(input !== 0){
+        input= parseFloat(input);
+        arr.push(input);
+        // console.log(input);
+        // console.log(arr);
+    }
+    for(let i=2 ; i<arr.length; i++){
+    
+    switch(arr[i-1]){
+        case '+' : 
+        arr[0] = arr[0] + arr[i];
+        break; 
+        case '-' : 
+        arr[0] = arr[0] - arr[i];
+        break;
+        case '*' : 
+        arr[0] = arr[0] * arr[i];
+        break;  
+        //console.log(arr);
+    }
+}
+ecran.value = arr[0];
 
-// parseFloat(ecran.value)
-
-    // function effacer() 
-    // { 
-    //     document.getElementById("output").value = "" 
-    // } 
+}
