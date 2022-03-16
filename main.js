32ca013c5cd0dc518e132790291f78bb19fe4b87
khalid hagane <khalidhagane62@gmail.com>
2022-03-16 21:19:03 +0100

    ajoute function suprimer dernier number

--- main.js
+++ main.js
@@ -4,10 +4,13 @@ let arr = [];
 let input = 0;
 
 function affichage(number){
-    ecran.value = ecran.value + number
-    input = input + number
-    
-    // (input + number) => fkola mara hat liya number kitzad 3la number li 9ablo okithat liya f case wahda ma array 
+    if(ecran.value==0){
+        ecran.value=number
+    }
+    else{
+        ecran.value += number
+    }
+    input += number 
     // console.log(input)
 }
 
@@ -15,18 +18,19 @@ let efacer = document.getElementById('suprimer')
 efacer.addEventListener('click',function()
 // function suprimer()
 {
-    ecran.value = "";
+    ecran.value = 0;
     input= 0;
     arr = [];
     // console.log(input)
 })
 
 let operateurs = document.querySelectorAll('.operateur')
-operateurs.forEach(traite => {
+operateurs.forEach(traite => 
+    {
     traite.addEventListener('click',function(){
         let content = traite.getAttribute('content')
         // function traiter(operateur)
-        if(input !== 0 ){
+        if(input !== ""){
         //ecran.value = ecran.value + operateur; 
         ecran.value+=content;
         input= parseFloat(input);
@@ -35,12 +39,30 @@ operateurs.forEach(traite => {
         input = 0;
         // console.log(arr);
         // console.log(input);
-    }
+        }
     });
 });
 
+// let point = document.getElementById('point')
+// point.addEventListener('click',function(){
+//     if(ecran.value !== ""){  
+//     }
+// }
+// )
+
+let suprimer_number = document.getElementById('sup_number')
+suprimer_number.addEventListener('click',function(){
+    // ecran.value = ecran.value.slice(0,-1)
+    //  arr = arr.slice(0,-1)
+    input= parseFloat(input);
+    arr.push(input)
+    ecran.value = ecran.value.slice(0,-1)
+    arr.pop()
+    // arr.pop() => proplime pop() kan tmssah liya number man tableau imakatmsahch liya operateur
+})
+
 function egale(arr){
-    if(input !== 0){
+    if(input !== ""){
         input= parseFloat(input);
         arr.push(input);
         // console.log(input);
@@ -57,6 +79,9 @@ function egale(arr){
         break;
         case '*' : 
         arr[0] = arr[0] * arr[i];
+        break; 
+        case '/' : 
+        arr[0] = arr[0] / arr[i];
         break;  
         //console.log(arr);
     }
